0b8137ccb26a7282443e0b04aa1c7cdff58e2213
Fazle Rabby Talukder <talukderfazlerabby@gmail.com>
2024-03-29 23:02:45 +0600

    photo feed

---
+++ app/[lang]/photos/[id]/page.js
@@ -0,0 +1,7 @@
+import PhotoDetails from "@/app/components/PhotoDetails";
+
+export default function PhotoDetailsPage({ params: { id, lang } }) {
+    return (
+        <PhotoDetails id={id} lang={lang} />
+    );
+}
